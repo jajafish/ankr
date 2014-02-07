c9b46d67906bbb6a0a6f2fce5f9742406017a353
JAJAFISH <jfish@gigbear.com>
2014-02-06 21:42:41 -0800

    intentions page shows relevant intentions

--- app/controllers/intentions_controller.rb
+++ app/controllers/intentions_controller.rb
@@ -1,5 +1,5 @@
 class IntentionsController < ApplicationController
-
+	include IntentionsHelper
 
 	# before_filter :check_intention_owner, only: [:destroy, :update, :edit]
 
@@ -48,47 +48,18 @@ class IntentionsController < ApplicationController
 
 	def show
 		@intention = Intention.find(params[:id])
-		@intentions_intentions = associate_intentions(@intention)
+		@intentions_intentions = find_intentions_from_an_intentions_words(@intention)
 
 	end
 
 
-	def associate_words(intention)
-
-		response = Unirest::post "https://twinword-topic-tagging.p.mashape.com/generate/", 
-	  		headers: { 
-	    		"X-Mashape-Authorization" => "5C6dpbqx8WPB9mcS1VKusvceo7B5wT3t"
-	  		},
-	  		parameters: { 
-	   			"text" => @intention.name
-	 		}
-
-	 	response.body["topic"].each do |k,v|
-
-	  		# check if this word exists in the word table,
-	  		# if it does, add it to the intention being created
-	  		# if not, create it and then add it
-
-	  		word = Word.where(:name => k)
-
-			if word.blank?
-				new_word = Word.create(name: k)
-				@intention.words << new_word
-			else
-				@intention.words << word
-	  		end
-	  	end
-
-	end
+	
 
 
-	def associate_intentions(intention)
-		@words_intentions = []
-		intention.words.each do |word|
-			@words_intentions << word.intentions
-		end
-		return @words_intentions
-	end
+
+
+
+	
 
 
 
